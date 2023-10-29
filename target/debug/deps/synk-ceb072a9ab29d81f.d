3f3dbd7d380bcf4b0240572f7a76f948f28680ce
Anant Narayan <anantnrg@gmail.com>
2023-10-29 15:39:44 +0530

    :sparkles: Initial commit

---
+++ target/debug/deps/synk-ceb072a9ab29d81f.d
@@ -0,0 +1,5 @@
+/home/mik3y/projects/Repos/synk/target/debug/deps/synk-ceb072a9ab29d81f: synk/src/main.rs
+
+/home/mik3y/projects/Repos/synk/target/debug/deps/synk-ceb072a9ab29d81f.d: synk/src/main.rs
+
+synk/src/main.rs:
---
+++ target/debug/synk.d
@@ -0,0 +1,1 @@
+/home/mik3y/projects/Repos/synk/target/debug/synk: /home/mik3y/projects/Repos/synk/synk/src/main.rs
