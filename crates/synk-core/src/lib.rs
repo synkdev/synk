97cce3d11325464579f0fe23b18e5cf9fc0d89b9
Anant Narayan <anantnrg@gmail.com>
2024-01-25 21:05:51 +0530

    add sample command enum

--- crates/synk-core/src/lib.rs
+++ crates/synk-core/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum Command {
+    EnterInsert,
+    EnterCmd,
+    EnterSelection,
+    Exit,
 }
