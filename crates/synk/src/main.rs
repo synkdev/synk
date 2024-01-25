fcba7ce94e6ac2f009aacc6ca9a0d99e2204b492
Anant Narayan <anantnrg@gmail.com>
2024-01-25 22:18:30 +0530

    simple app works

--- crates/synk/src/main.rs
+++ crates/synk/src/main.rs
@@ -1,3 +1,21 @@
+use freya::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    render!(
+        rect {
+            height: "100%",
+            width: "100%",
+            background: "rgb(35, 35, 35)",
+            color: "white",
+            padding: "12",
+            onclick: move |_| count += 1,
+            label { "Click to increase -> {count}" }
+        }
+    )
 }
