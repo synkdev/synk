afc65ba2b38b147c84dc09e0ecd7abc8dd8816c6
Anant Narayan <anantnrg@gmail.com>
2024-01-30 15:13:35 +0530

    add Ui struct and some functions

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,7 +1,25 @@
 use eframe::NativeOptions;
+use egui::ViewportBuilder;
 
-pub struct Ui;
+pub struct Ui {
+    title: &'static str,
+    app_id: &'static str,
+}
 
 impl Ui {
-    pub fn native_options() -> NativeOptions {}
+    pub fn native_options(&self) -> NativeOptions {
+        NativeOptions {
+            viewport: ViewportBuilder::default()
+                .with_inner_size([1200, 700])
+                .with_title(self.title)
+                .with_app_id(self.app_id)
+                .with_active(true)
+                .with_visible(true)
+                .with_decorations(false),
+            ..Default::default()
+        }
+    }
+    pub fn launch(&self, native_options: NativeOptions) -> anyhow::Result<()> {
+        eframe::run_native(self.title, native_options, None)
+    }
 }
