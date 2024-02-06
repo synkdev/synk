d8b8de5b5b97955d3fe5b737985b0875fd165a3b
Anant Narayan <anantnrg@gmail.com>
2024-02-06 20:20:00 +0530

    remove dioxus-desktop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() -> anyhow::Result<()> {
-    let native_options = ui::native_options("Synk Editor", "synk");
-    ui::launch(native_options)?;
+    dioxus_desktop::launch(App);
     Ok(())
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
 }
