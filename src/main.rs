8fabf52e848256a1c36ff6ac92bbceb36b4bd4f0
Anant Narayan <anantnrg@gmail.com>
2024-02-07 09:27:12 +0530

    Add simple freya app

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-#![allow(non_snake_case)]
-use dioxus::prelude::*;
+use freya::prelude::*;
 
-fn main() -> anyhow::Result<()> {
-    dioxus_desktop::launch(App);
-    Ok(())
+fn main() {
+    launch(app);
 }
 
-fn App(cx: Scope) -> Element {
-    cx.render(rsx! {
-        div {
-            "Hello, world!"
+fn app() -> Element {
+    let mut times = use_signal(|| 1);
+
+    let exclamations = "!".repeat((*times)());
+
+    rsx!(
+        rect {
+            width: "100%",
+            height: "100%",
+            background: "rgb(0, 109, 119)",
+            direction: "vertical",
+            main_align: "center",
+            cross_align: "center",
+            onclick: move |_| times += 1,
+            label {
+                width: "100%",
+                font_size: "50",
+                text_align: "center",
+                color: "white",
+                "Hello, World{exclamations}"
+            }
         }
-    })
+    )
 }
