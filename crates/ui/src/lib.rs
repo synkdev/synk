db27b5cdf9e2aa83e80dd7f35f1c0cb34b9aa2f7
Anant Narayan <anantnrg@gmail.com>
2024-02-07 09:36:31 +0530

    Add SynkUi struct

---
+++ crates/ui/src/lib.rs
@@ -0,0 +1,22 @@
+use freya::prelude::*;
+
+pub struct SynkUI;
+
+impl SynkUI {
+    pub fn render() -> Element {
+        let color = "rgb(10, 20, 10)";
+        rsx! {
+            rect {
+                width: "100%",
+                height: "100%",
+                direction: "vertical",
+                background: "{color}",
+                main_align: "center",
+                cross_align: "center",
+                label {
+                    "hello"
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
 use freya::prelude::*;
+use ui::SynkUI;
 
 fn main() {
-    launch(app);
-}
-
-fn app() -> Element {
-    let mut times = use_signal(|| 1);
-
-    let exclamations = "!".repeat((*times)());
-
-    rsx!(
-        rect {
-            width: "100%",
-            height: "100%",
-            background: "rgb(0, 109, 119)",
-            direction: "vertical",
-            main_align: "center",
-            cross_align: "center",
-            onclick: move |_| times += 1,
-            label {
-                width: "100%",
-                font_size: "50",
-                text_align: "center",
-                color: "white",
-                "Hello, World{exclamations}"
-            }
-        }
-    )
+    launch(SynkUI::render);
 }
