1edea08f8a5a8ebe13a9423ecd54bfb3cfe27bd7
Anant Narayan <anantnrg@gmail.com>
2024-02-07 14:52:09 +0530

    Add window config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use freya::prelude::*;
 
 fn main() {
-    launch(ui::SynkUI);
+    launch_cfg(
+        ui::SynkUI,
+        LaunchConfig::<()>::builder()
+            .with_height(700.0)
+            .with_width(1200.0)
+            .with_title("Synk Editor")
+            .build(),
+    );
 }
