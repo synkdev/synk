db1fc4df37e2dd6cb14aa933ffd09a6c7e4da8d6
Anant Narayan <anantnrg@gmail.com>
2024-02-08 21:14:56 +0530

    Add HorizontalSeparator

--- crates/ui/src/separator.rs
+++ crates/ui/src/separator.rs
@@ -29,3 +29,33 @@ pub fn VerticalSeparator() -> Element {
         }
     }
 }
+
+#[allow(non_snake_case)]
+#[component]
+pub fn HorizontalSeparator() -> Element {
+    let hover_anim = use_animation_transition(TransitionAnimation::SineInOut(150), (), |_| {
+        vec![
+            Transition::new_color("rgb(69, 71, 90)", "rgb(243, 139, 168)"),
+            Transition::new_size(2.0, 6.0),
+        ]
+    });
+
+    let color = hover_anim.get(0).unwrap().as_color();
+    let height = hover_anim.get(1).unwrap().as_size();
+
+    rsx! {
+        rect {
+            width: "100%",
+            height: "{height}",
+            background: "{color}",
+            onmouseover: {
+                to_owned![hover_anim];
+                move |_| { hover_anim.start() }
+            },
+            onmouseleave: {
+                to_owned![hover_anim];
+                move |_| { hover_anim.reverse() }
+            }
+        }
+    }
+}
