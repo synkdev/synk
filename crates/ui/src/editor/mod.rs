1659113e5dc9545fee2bffe19f7edafb9ded5b69
Anant Narayan <anantnrg@gmail.com>
2024-02-09 08:53:57 +0530

    Correctly calculate editor height

--- crates/ui/src/editor/mod.rs
+++ crates/ui/src/editor/mod.rs
@@ -5,5 +5,5 @@ use crate::colors::EditorColors;
 #[allow(non_snake_case)]
 #[component]
 pub fn Editor(colors: EditorColors) -> Element {
-    rsx! { rect { background: "{colors.background}", width: "100%", height: "100%" } }
+    rsx! {rect { background: "{colors.background}", width: "100%", height: "calc(100% - 84)" }}
 }
