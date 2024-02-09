baed9149620faae5bee443a8013be36f3fb7a69d
Anant Narayan <anantnrg@gmail.com>
2024-02-09 12:09:54 +0530

    Make HorizontalSeparator draggable

--- crates/ui/src/separator.rs
+++ crates/ui/src/separator.rs
@@ -21,16 +21,16 @@ pub fn VerticalSeparator(
         let color = hover_anim.get(0).unwrap().as_color();
         let width = hover_anim.get(1).unwrap().as_size();
 
-        let mut hovering = use_signal(|| false);
+        let mut is_hovered = use_signal(|| false);
         let mut position = use_signal(|| 0usize);
-        let mut clicking = use_signal(|| false);
+        let mut is_clicked = use_signal(|| false);
 
         let onmouseleave = {
             to_owned![hover_anim];
             move |_: MouseEvent| {
                 hover_anim.reverse();
-                hovering.set(false);
-                clicking.set(false);
+                is_hovered.set(false);
+                is_clicked.set(false);
             }
         };
 
@@ -38,8 +38,8 @@ pub fn VerticalSeparator(
             to_owned![hover_anim];
             move |e: MouseEvent| {
                 hover_anim.start();
-                hovering.set(true);
-                if *clicking.read() {
+                is_hovered.set(true);
+                if *is_clicked.read() {
                     let x = e.get_screen_coordinates().x as usize;
                     if let Some(mut callback) = callback {
                         let extend_size = x.saturating_sub(*position.read());
@@ -56,11 +56,11 @@ pub fn VerticalSeparator(
 
         let onmousedown = move |e: MouseEvent| {
             position.set(e.get_screen_coordinates().x as usize);
-            clicking.set(true);
+            is_clicked.set(true);
         };
 
         let onclick = move |_: MouseEvent| {
-            clicking.set(false);
+            is_clicked.set(false);
         };
 
         rsx! {
@@ -83,7 +83,11 @@ pub fn VerticalSeparator(
 
 #[allow(non_snake_case)]
 #[component]
-pub fn HorizontalSeparator(colors: SeparatorColors, interactive: bool) -> Element {
+pub fn HorizontalSeparator(
+    colors: SeparatorColors,
+    interactive: bool,
+    mut callback: Option<Signal<usize>>,
+) -> Element {
     if interactive {
         let hover_anim = use_animation_transition(TransitionAnimation::SineInOut(150), (), |_| {
             vec![
@@ -95,19 +99,59 @@ pub fn HorizontalSeparator(colors: SeparatorColors, interactive: bool) -> Elemen
         let color = hover_anim.get(0).unwrap().as_color();
         let height = hover_anim.get(1).unwrap().as_size();
 
+        let mut is_hovered = use_signal(|| false);
+        let mut position = use_signal(|| 0usize);
+        let mut is_clicked = use_signal(|| false);
+
+        let onmouseleave = {
+            to_owned![hover_anim];
+            move |_: MouseEvent| {
+                hover_anim.reverse();
+                is_hovered.set(false);
+                is_clicked.set(false);
+            }
+        };
+
+        let onmouseover = {
+            to_owned![hover_anim];
+            move |e: MouseEvent| {
+                hover_anim.start();
+                is_hovered.set(true);
+                if *is_clicked.read() {
+                    let x = e.get_screen_coordinates().x as usize;
+                    if let Some(mut callback) = callback {
+                        let extend_size = x.saturating_sub(*position.read());
+                        if extend_size == 0 {
+                            *callback.write() -= 1;
+                        } else {
+                            *callback.write() += 1;
+                        }
+                        position.set(e.get_screen_coordinates().x as usize);
+                    }
+                }
+            }
+        };
+
+        let onmousedown = move |e: MouseEvent| {
+            position.set(e.get_screen_coordinates().x as usize);
+            is_clicked.set(true);
+        };
+
+        let onclick = move |_: MouseEvent| {
+            is_clicked.set(false);
+        };
+
         rsx! {
             rect {
-                width: "100%",
-                height: "{height}",
-                background: "{color}",
-                onmouseover: {
-                    to_owned![hover_anim];
-                    move |_| { hover_anim.start() }
-                },
-                onmouseleave: {
-                    to_owned![hover_anim];
-                    move |_| { hover_anim.reverse() }
-                }
+                width: "10",
+                height: "100%",
+                onmouseover: onmouseover,
+                onmouseleave: onmouseleave,
+                onclick: onclick,
+                onmousedown: onmousedown,
+                direction: "vertical",
+                rect { width: "100%", height: "calc(100% - {height})" }
+                rect { width: "100%", height: "{height}", background: "{color}" }
             }
         }
     } else {
