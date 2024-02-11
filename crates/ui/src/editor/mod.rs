52efbff3dcb39aa870d528d55c79444efd681648
Anant Narayan <anantnrg@gmail.com>
2024-02-11 12:03:30 +0530

    Add basic treesitter example

--- crates/ui/src/editor/mod.rs
+++ crates/ui/src/editor/mod.rs
@@ -4,10 +4,22 @@ use freya::prelude::*;
 
 use crate::{colors::Colors, editor::gutter::Gutter, separator::VerticalSeparator};
 use crop::Rope;
+use tree_sitter::{Language, Parser};
 
 #[allow(non_snake_case)]
 #[component]
 pub fn Editor(colors: Colors, contents: Rope) -> Element {
+    let mut parser = Parser::new();
+    parser
+        .set_language(tree_sitter_rust::language())
+        .expect("Error loading Rust grammar");
+    let tree = parser.parse(contents.to_string(), None).unwrap();
+    let mut cursor = tree.root_node().walk();
+
+    for node in tree.root_node().children(&mut cursor) {
+        println!("{}", node.kind());
+    }
+
     rsx! {
         rect {
             background: "{colors.editor.background}",
