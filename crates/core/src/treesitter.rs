b787baf5b91eba15d726e812fec9269315ce138a
Anant Narayan <anantnrg@gmail.com>
2024-02-13 12:00:48 +0530

    ChunkBytes

--- crates/core/src/treesitter.rs
+++ crates/core/src/treesitter.rs
@@ -1,3 +1,12 @@
-struct ChunkBytes {
-    chunks: crop
+use crop::iter::Chunks;
+
+struct ChunkBytes<'a> {
+    chunks: Chunks<'a>,
+}
+
+impl<'a> Iterator for ChunkBytes<'a> {
+    type Item = &'a [u8];
+    fn next(&mut self) -> Option<Self::Item> {
+        self.chunks.next().map(str::as_bytes)
+    }
 }
