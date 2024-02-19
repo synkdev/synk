d3cc3277061e4ab03d4e9f4e9c2e6f47a827e639
Anant Narayan <anantnrg@gmail.com>
2024-02-19 21:59:34 +0530

    Move EditorConfig to struct

--- crates/ui/src/editor/mod.rs
+++ crates/ui/src/editor/mod.rs
@@ -10,44 +10,40 @@ use synk_core::document::Document;
 
 use crate::{colors::Colors, editor::gutter::Gutter, separator::VerticalSeparator};
 
+#[derive(Clone, PartialEq)]
+pub struct EditorConfig {
+    pub document: Document,
+    pub line_height: f32,
+    pub font_family: &'static str,
+    pub font_size: f32,
+}
+
 #[allow(non_snake_case)]
 #[component]
-pub fn Editor(
-    colors: Colors,
-    line_height: f32,
-    font_family: &'static str,
-    font_size: f32,
-) -> Element {
-    let document = Document::new(
-        "fn main() {\n    let x = \"Hello!\\n\";\n    println!(\"{x}\");\n}".to_string(),
-    );
-
+pub fn Editor(colors: Colors, config: EditorConfig) -> Element {
     let platform = use_platform();
     use_effect(move || {
         platform.send(EventMessage::RequestRerender).unwrap();
     });
 
-    let canvas = use_canvas(
-        &(document, font_size, font_family, line_height),
-        |document, font_size, font_family, line_height| {
-            Box::new(move |canvas, _, region| {
-                let rope = document.rope.clone();
-                canvas.translate((region.min_x(), region.min_y()));
+    let canvas = use_canvas(&config, |config| {
+        Box::new(move |canvas, _, region| {
+            let rope = config.document.rope.clone();
+            canvas.translate((region.min_x(), region.min_y()));
 
-                let mut paint = Paint::default();
-                paint.set_anti_alias(true);
-                paint.set_color(Color::WHITE);
+            let mut paint = Paint::default();
+            paint.set_anti_alias(true);
+            paint.set_color(Color::WHITE);
 
-                let font_style = FontStyle::new(Weight::NORMAL, Width::NORMAL, Slant::Upright);
-                let font_family = FontMgr::new()
-                    .match_family_style(font_family, font_style)
-                    .unwrap();
-                let font = Font::from_typeface(font_family, font_size);
+            let font_style = FontStyle::new(Weight::NORMAL, Width::NORMAL, Slant::Upright);
+            let font_family = FontMgr::new()
+                .match_family_style(config.font_family, font_style)
+                .unwrap();
+            let font = Font::from_typeface(font_family, config.font_size);
 
-                canvas.restore();
-            })
-        },
-    );
+            canvas.restore();
+        })
+    });
 
     rsx! {
         rect {
@@ -55,7 +51,11 @@ pub fn Editor(
             width: "100%",
             height: "calc(100% - 84)",
             direction: "horizontal",
-            Gutter { rope: document.rope.clone(), colors: colors.line_numbers, line_height }
+            Gutter {
+                rope: config.document.rope.clone(),
+                colors: colors.line_numbers,
+                line_height: config.line_height
+            }
             VerticalSeparator { interactive: false, reverse: false, colors: colors.separator }
             rect {
                 width: "calc(100% - 50)",
--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -7,9 +7,10 @@ pub mod tab_bar;
 
 use colors::Colors;
 use freya::prelude::*;
+use synk_core::document::Document;
 
 use crate::{
-    editor::Editor,
+    editor::{Editor, EditorConfig},
     separator::{HorizontalSeparator, VerticalSeparator},
     sidebar::Sidebar,
     statusbar::Statusbar,
@@ -20,31 +21,42 @@ use crate::{
 pub fn SynkUI() -> Element {
     let colors = Colors::new();
     let sidebar_width = use_signal(|| 300_isize);
+    let document = Document::new(
+        "fn main() {\n    let x = \"Hello!\\n\";\n    println!(\"{x}\");\n}".to_string(),
+    );
 
     rsx! {
-        rect {
-            width: "100%",
-            height: "100%",
-            direction: "horizontal",
-            background: "{colors.background}",
-            color: "{colors.foreground}",
-            cross_align: "center",
-            font_family: "JetBrains Mono",
-            overflow: "clip",
-            Sidebar { width: sidebar_width, colors: colors.clone().sidebar }
-            VerticalSeparator {
-                colors: colors.separator.clone(),
-                interactive: true,
-                callback: sidebar_width,
-                reverse: false
-            }
-            rect { width: "calc(100% - {sidebar_width})", height: "100%", direction: "vertical",
-                TabBar { colors: colors.clone().tab_bar }
-                HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
-                Editor { colors: colors.clone(), line_height: 36.0, font_family: "JetBrains Mono", font_size: 16.0 }
-                HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
-                Statusbar { colors: colors.statusbar }
+            rect {
+                width: "100%",
+                height: "100%",
+                direction: "horizontal",
+                background: "{colors.background}",
+                color: "{colors.foreground}",
+                cross_align: "center",
+                font_family: "JetBrains Mono",
+                overflow: "clip",
+                Sidebar { width: sidebar_width, colors: colors.clone().sidebar }
+                VerticalSeparator {
+                    colors: colors.separator.clone(),
+                    interactive: true,
+                    callback: sidebar_width,
+                    reverse: false
+                }
+                rect { width: "calc(100% - {sidebar_width})", height: "100%", direction: "vertical",
+                    TabBar { colors: colors.clone().tab_bar }
+                    HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
+                    Editor {
+                        colors: colors.clone(),
+                        config: EditorConfig {
+        line_height: 36.0,
+        font_family: "JetBrains Mono",
+        font_size: 16.0,
+        document,
+    }
+                    }
+                    HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
+                    Statusbar { colors: colors.statusbar }
+                }
             }
         }
-    }
 }
