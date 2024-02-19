89c01ef90e24892f2bfe46d960262004deefa23f
Anant Narayan <anantnrg@gmail.com>
2024-02-19 22:53:00 +0530

    Measure text width

--- crates/ui/src/editor/mod.rs
+++ crates/ui/src/editor/mod.rs
@@ -3,6 +3,7 @@ pub mod gutter;
 
 use freya::torin::*;
 use freya::{common::EventMessage, prelude::*};
+use skia_safe::TextBlob;
 use skia_safe::{
     font_style::{Slant, Weight, Width},
     Color, Font, FontMgr, FontStyle, Paint,
@@ -41,13 +42,21 @@ pub fn Editor(colors: Colors, config: EditorConfig) -> Element {
                 .match_family_style(config.font_family, font_style)
                 .unwrap();
             let font = Font::from_typeface(font_family, config.font_size);
+            let mut next_line_start = region.min_y();
 
-            for line in rope.lines() {
+            for (line_idx, line) in rope.lines().enumerate() {
                 let torin_rect = Node::from_size_and_direction(
                     Size::Pixels(Length::new(region.width())),
                     Size::Pixels(Length::new(config.line_height)),
                     DirectionMode::Horizontal,
                 );
+                let mut next_char_start = region.min_x();
+                for (byte_idx, char) in line.chars().enumerate() {
+                    let text_blob = TextBlob::from_str(char.to_string().as_str(), &font);
+                    let text_bounds = font.measure_text(char.to_string().as_str(), Some(&paint)).1;
+                    println!("{}: {}", byte_idx, text_bounds.width());
+                }
+                next_line_start += config.line_height;
             }
 
             canvas.restore();
