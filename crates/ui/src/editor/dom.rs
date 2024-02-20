34d952492e9a1ff298e96eb9e3b45fe3722e7182
Anant Narayan <anantnrg@gmail.com>
2024-02-20 11:42:49 +0530

    test

--- crates/ui/src/editor/dom.rs
+++ crates/ui/src/editor/dom.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
-use freya::torin::node::Node;
+use freya::torin::{dom_adapter::DOMAdapter, node::Node};
 
 #[derive(Clone)]
 pub struct EditorLine {
+    pub parent: Option<Node>,
     pub children: Vec<Node>,
     pub node: Node,
     pub height: f32,
@@ -15,10 +16,19 @@ pub struct EditorDom {
 }
 
 impl EditorDom {
-    pub fn add(&mut self, line_id: usize, children: Vec<Node>, line: Node, line_height: f32) {
+    #[inline]
+    pub fn add(
+        &mut self,
+        line_id: usize,
+        children: Vec<Node>,
+        line: Node,
+        line_height: f32,
+        parent: Option<Node>,
+    ) {
         self.lines.insert(
             line_id,
             EditorLine {
+                parent,
                 children,
                 height: line_height,
                 node: line,
